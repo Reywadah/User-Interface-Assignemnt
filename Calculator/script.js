12739d53b522995ebff6b6d45452c932fc3bc468
Reywadah <wwaaddaah@gmail.com>
2023-07-17 17:42:36 +0400

    Calculator project from TheOdinProject course.

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,65 +1,9 @@
-let firstNumber = "";
-let secondNumber = 0;
+let firstNumber='';
+let secondNumber='';
 let operator;
-let DIsplay = document.querySelector('#display')
-const display = document.createElement('p')
-DIsplay.appendChild(display);
 
-let finalResult = document.querySelector('#result');
-finalResult.addEventListener('click', () => {result()})
-
-let addition = document.querySelector('#add');
-addition.addEventListener('click', () => {addNumbers(); console.log(firstNumber);displayNumber();})
-
-let subtraction = document.querySelector('#sub');
-subtraction.addEventListener('click', () => {subNumbers();displayNumber();})
-
-display.textContent = "0";
-
-function operate(n1,n2,operator){
-    
-}
-
-function getFirstNumber(x){
-    firstNumber += x;
-}
-
-function clearDisplay(){
-    firstNumber="";
-}
-
-function delLastNumber(){
-    let deleted = firstNumber.split("");
-    deleted.pop();
-    firstNumber = deleted.join("");
-    displayNumber();
-}
-
-function addNumbers(){
-    secondNumber += +firstNumber;
-    firstNumber = "";
-}
-
-function subNumbers(){
-    if(firstNumber != NaN){
-    secondNumber = +firstNumber;
-
-    // console.log(firstNumber)
-    console.log(secondNumber)
-    firstNumber = "";}
-}
-
-function result(){
-    firstNumber = +firstNumber + +secondNumber;
-    firstNumber =` ${firstNumber}`
-    secondNumber=0;
-    displayNumber();
-}
-
-function displayNumber(){
-    display.textContent = firstNumber;
-    DIsplay.appendChild(display);
-}
+const dot = document.querySelector('#dot');
+dot.addEventListener('click', () => {dotNotation();})
 
 const clear= document.querySelector('#clear')
 clear.addEventListener('click', () => {clearDisplay(); console.log(firstNumber);displayNumber();})
@@ -67,8 +11,12 @@ clear.addEventListener('click', () => {clearDisplay(); console.log(firstNumber);
 const del = document.querySelector('#del')
 del.addEventListener('click', () => {delLastNumber();console.log(firstNumber);displayNumber();})
 
+const add0 = document.querySelector('#no0');
+add0.addEventListener('click', () => {getFirstNumber("0");
+console.log(firstNumber);displayNumber();;})
+
 const add1 = document.querySelector('#no1');
-add1.addEventListener('click', () => {getFirstNumber("1");displayNumber();
+add1.addEventListener('click', () => {getFirstNumber("1");
 console.log(firstNumber);displayNumber();;})
 
 const add2 = document.querySelector('#no2');
@@ -103,6 +51,100 @@ const add9 = document.querySelector('#no9');
 add9.addEventListener('click', () => {getFirstNumber("9");
 console.log(firstNumber);displayNumber();;})
 
-function sum(){
+let DIsplay = document.querySelector('#display')
+const display = document.createElement('p')
+DIsplay.appendChild(display);
+
+let finalResult = document.querySelector('#result');
+finalResult.addEventListener('click', () => {console.log(firstNumber);console.log(operator);console.log(secondNumber);firstNumber = operate(firstNumber,secondNumber,operator);displayNumber();})
+
+let addition = document.querySelector('#add');
+addition.addEventListener('click', () => {operator = '+';
+console.log(operator);joinNumbers();})
+
+let subtraction = document.querySelector('#sub');
+subtraction.addEventListener('click', () => {operator = '-';
+console.log(operator);joinNumbers();})
+
+let multiplication = document.querySelector('#mult');
+multiplication.addEventListener('click', () => {operator = '*';
+console.log(operator);joinNumbers();})
+
+let division = document.querySelector('#div');
+division.addEventListener('click', () => {operator = '/';
+console.log(operator);joinNumbers();})
+
+display.textContent = "0";
+
+function sum(a,b){
+    let fix = +b + +a;
+    return fix.toFixed(2);
+}
+
+function sub(a,b){
+    let fix = b-a;
+    return fix.toFixed(2);
+}
+
+function mult(a,b){
+    let fix = b*a;
+    return fix.toFixed(2);
+}
+
+function div(a,b){
+    let fix = b/a;
+    return fix.toFixed(2);
+}
+
+function operate(a,b,$){
+    switch ($) {
+        case '+':
+            return sum(a,b);
+            break;
+        case '-':
+            return sub(a,b);
+            break;
+        case '*':
+            return mult(a,b);
+            break;
+        case '/':
+            return div(a,b);
+            break;
     
+        default:
+            break;
+    }
+}
+function joinNumbers(){
+    secondNumber = firstNumber;
+    firstNumber = "";
+}
+
+function displayNumber(){
+    display.textContent = firstNumber;
+    DIsplay.appendChild(display);
+}
+
+function getFirstNumber(x){
+    firstNumber += x;
+}
+
+function clearDisplay(){
+    firstNumber="";
+    secondNumber='';
+}
+
+function delLastNumber(){
+    let deleted = firstNumber.split("");
+    deleted.pop();
+    firstNumber = deleted.join("");
+    displayNumber();
+}
+
+function dotNotation(){
+    let deleted = firstNumber.split("");
+    if(deleted.includes('.',0)){return}
+    deleted.push('.');
+    firstNumber = deleted.join("");
+    displayNumber();
 }
